e695f49822a82e978ce27163f7ffae76cfb18334
Marcin Trawiński <trawinski1@wp.pl>
2018-05-06 15:09:58 +0200

    Add post method to HTTP object and submitPost to index.js

--- src/http.js
+++ src/http.js
@@ -4,6 +4,17 @@ class HTTP {
     const responseData = await response.json();
     return responseData;
   }
+
+  async post(url, data) {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-type': 'application/json'},
+      body: JSON.stringify(data)
+    });
+
+    const responseData = await response.json();
+    return responseData;
+  }
 }
 
 export const http = new HTTP();
--- src/index.js
+++ src/index.js
@@ -4,9 +4,24 @@ import { ui } from './ui';
 
 document.addEventListener('DOMContentLoaded', getPosts);
 
+ui.postBtn.addEventListener('click', submitPost);
+
 function getPosts(url) {
   http.get('http://localhost:3000/posts')
     .then(data => ui.showPosts(data))
     .catch(err => console.log(err))
+}
+
+function submitPost() {
+  const newPost = {
+    title: ui.titleInput.value,
+    body: ui.bodyInput.value
+  }
+
+  http.post('http://localhost:3000/posts', newPost)
+    .then(data => {
+      getPosts();
+    })
+    .catch(err => console.log(err));
 }
 
